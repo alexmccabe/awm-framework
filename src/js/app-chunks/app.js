16951815d097b9d43e66f23d1cb6c2fac4d56060
Alex McCabe <alexwmccabe@gmail.com>
2017-07-15 16:05:34 +0100

    Update input logic of the application

---
+++ src/js/app-chunks/app.js
@@ -0,0 +1,13 @@
+import bootstrap from '@/js/bootstrap.js';
+
+/**
+ * Immediately Invoked Function to launch the JS on this page. This isolates the
+ * application from other JS that may be running on the page
+ * @param  {object} root    The global root. Most often it is the Window object
+ * @return {function}
+ */
+(function (root) {
+
+    // App / Page logic goes here
+
+} (window));
